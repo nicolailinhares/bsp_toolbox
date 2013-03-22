bf28c4537dcb47448783569c734247ed44455b90
unknown <nicolai.linhares@gmail.com>
2013-03-21 21:13:14 -0300

    Correcao calculo da mediana

--- toolbox/feature_extraction/frequency/bsp_bandwidth.m
+++ toolbox/feature_extraction/frequency/bsp_bandwidth.m
@@ -1,4 +1,4 @@
-function [bel, up, med] = bsp_bandwidth(signal, Fs)
+function [fl, fh, med] = bsp_bandwidth(signal, Fs)
 %% Function name....: bsp_bandwidth
 % Date.............: February 20, 2013
 % Author...........: Nicolai Diniz Linhares
@@ -16,8 +16,8 @@ function [bel, up, med] = bsp_bandwidth(signal, Fs)
 %                    t = (0:500)*0.001; 
 %                    y1 = sin(2*pi*60*t) + 0.8*sin(2*pi*80*t);
 %                    y2 = sin(2*pi*120*t) + 0.7*sin(2*pi*140*t);
-%                    [bel,up,med] = bsp_bandwidth(y1,1000);
-%                    [bel2,up2,med2] = bsp_bandwidth(y2,1000);
+%                    [fl,fh,med] = bsp_bandwidth(y1,1000);
+%                    [fl2,fh2,med2] = bsp_bandwidth(y2,1000);
 %take at maximum 20000 samples, for performance sake
 l = numel(signal);
 if l > 20000
@@ -26,17 +26,17 @@ if l > 20000
     signal = signal(b:u);
 end
 %calculate the power estimate
-[Px, x] = pwelch(signal, 33, 32, [], Fs, 'onesided');
+[Px, x] = pwelch(signal, [], [], [], Fs, 'onesided');
 %scale the units for dB
 P = 10*log10(Px);
 %find the points where the power is 3dB lower than the max
 max_power = max(P);
 indx = find(P >= max_power - 3);
-bel = x(indx(1));
-up = x(indx(numel(indx)));
-if nargout == 0
-    plot(x,P);
-elseif nargout == 3
+fl = x(indx(1));
+ind_fl = indx(1);
+fh = x(indx(numel(indx)));
+ind_fh = indx(numel(indx));
+if nargout ~= 2
     %insert an offset to prevent the area from being negative
     mini = min(P);
     if mini < 0
@@ -44,10 +44,10 @@ elseif nargout == 3
     end
     %calculates the area
     area = trapz(P);
-    tol = area*0.05;
+    tol = area*0.005;
     half_area = area/2;
     %set the variables for the binary search of the median frequency
-    upp = numel(x);
+    fhp = numel(x);
     half_point = floor(numel(x)/2);
     idx = half_point;
     bot = 0;
@@ -58,8 +58,8 @@ elseif nargout == 3
         f = x(idx);
         f_area = trapz(P(1:idx));
         if f_area > (half_area - tol)
-            upp = idx;
-            idx = floor((idx - bot)/2);
+            fhp = idx;
+            idx = idx - floor((idx - bot)/2);
             greater = 1;
         end
         if f_area < (half_area + tol)
@@ -67,9 +67,16 @@ elseif nargout == 3
                 break;
             end
             bot = idx;
-            idx = floor((upp - idx)/2);
+            idx = idx + floor((fhp - idx)/2);
         end
         greater = 0;
     end
     med = f;
+    if nargout == 0
+        P = P - abs(mini);
+        h = plot(x,P);
+        line(fl,P(ind_fl),'Marker','s','Color','red','LineWidth',2.0);
+        line(fh,P(ind_fh),'Marker','s','Color','red','LineWidth',2.0);
+        line([med,med],[mini, max_power],'Color','magenta');
+    end
 end
