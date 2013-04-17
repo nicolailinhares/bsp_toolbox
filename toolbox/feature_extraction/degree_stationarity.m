86f3c809c2de2afb22c97783d241504afb829a6f
unknown <nicolai.linhares@gmail.com>
2013-04-17 13:08:11 -0300

    Alteracoes recentes

--- toolbox/feature_extraction/degree_stationarity.m
+++
@@ -1,6 +0,0 @@
-function ds = degree_stationarity(signal)
-    L = max(size(signal));
-    h_transf = hilbert(signal);
-    n = (sum(abs(h_transf)))/L;
-    elem = (1 - (abs(h_transf)./n)).^2;
-    ds = sum(elem)/L;
