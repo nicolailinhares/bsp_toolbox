d1546afbe81df37eea8d3a2668eab9d9fd2ec0ff
Nicolai Linhares <nicolai.linhares@gmail.com>
2013-05-01 23:15:26 -0300

    Adicao de entropia aproximada e sample entropy

--- toolbox/feature_extraction/entropy/bsp_ap_en.m
+++ toolbox/feature_extraction/entropy/bsp_ap_en.m
@@ -1,31 +1,54 @@
-function ap_en = bsp_ap_en(signal,m,r,N)
+function ap_en = bsp_ap_en(signal,dim,r,down)
 %% Function name....: bsp_ap_en
-% Date.............: March 14, 2013
-% Author...........: Nicolai Diniz Linhares
+% Date.............: May 01, 2013
+% Author...........: Nicolai Diniz Linhares adapted from Kijoon Lee
 % Description......:
 %                    This function computes the aproximated entropy of a
 %                    signal
 % Parameters.......: 
 %                    signal .....-> input series
-%                    m ..-> size of the sequency to be compared
+%                    dimm ..-> size of the sequency to be compared
 %                    r ..-> similarity tolerance (0.1*SD < r < 0.25*SD)
-%                    N ..-> number of points of the series to be used
+%                    down(optional) ..-> ratio of downsampling of the
+%                    signal
 % Return...........:
 %                    ap_en .... -> aproximated entropy estimation
 % Usage............:
 %                    t = (0:500)*0.001; 
 %                    y1 = sin(2*pi*60*t) + 0.8*sin(2*pi*80*t);
 %                    y2 = sin(2*pi*120*t) + 0.7*sin(2*pi*140*t);
-%                    ap_en = bsp_ap_en(y1,2,0.15*std(y1),250);
-%                    ap_en = bsp_ap_en(y2,2,0.15*std(y2),250);
+%                    ap_en = bsp_ap_en(y1,2,0.15*std(y1));
+%                    ap_en = bsp_ap_en(y2,2,0.15*std(y2),2);
 
-if (length(signal) < N)
-    disp('the number points to be used is greater than the signal length')
-    N = length(signal);
+if nargin == 4
+    signal = downsample(signal,down);
 end
-u = signal(1:N);
 
-fi_m = bsp_fi_calc(m,r,N,u);
-fi_m2 = bsp_fi_calc(m+1,r,N,u);
+N = length(signal);
+result = zeros(1,2);
 
-ap_en = fi_m - fi_m2;
+for j = 1:2
+    m = dim+j-1;
+    q = N-m+1;
+    phi = zeros(1,q);
+    dataMat = zeros(m,q);
+    
+    % creating columns with groups of m elements of the signal
+    for i = 1:m
+        dataMat(i,:) = signal(i:N-m+i);
+    end
+    
+    % comparing the result of the subtraction of every group with r
+    for i = 1:q
+        tempMat = abs(dataMat - repmat(dataMat(:,i),1,q));
+        boolMat = any( (tempMat > r),1);
+        phi(i) = sum(~boolMat)/q;
+    end
+    
+    % summing over the counts
+    result(j) = sum(log(phi))/q;
+end
+
+ap_en = result(1)-result(2);
+
+end
--- toolbox/feature_extraction/stationarity/bsp_stat_sd.m
+++ toolbox/feature_extraction/stationarity/bsp_stat_sd.m
@@ -1,4 +1,4 @@
-function [me,sd,ds] = bsp_stat_sd(signal, t, Fs)
+function [me,sd,ds] = bsp_stat_sd(signal, T, Fs)
 %% Function name....: bsp_stat_sd
 % Date.............: February 20, 2013
 % Author...........: Nicolai Diniz Linhares
@@ -8,7 +8,7 @@ function [me,sd,ds] = bsp_stat_sd(signal, t, Fs)
 %                    time goes
 % Parameters.......: 
 %                    signal .....-> input series
-%                    t ..-> the period of time of each calculation
+%                    T ..-> the period of time of each calculation
 %                    Fs ..-> the sampling frequeny of the signal
 % Return...........:
 %                    me .... -> the standard deviation of the mean
@@ -19,8 +19,8 @@ function [me,sd,ds] = bsp_stat_sd(signal, t, Fs)
 % Usage............:
 %                    t = (0:500)*0.001; 
 %                    y1 = sin(2*pi*60*t);
-%                    [me,sd] = bsp_stat_sd(y1, 100);
-    n = t*Fs;
+%                    [me,sd,ds] = bsp_stat_sd(y1, 2, 1000);
+    n = T*Fs;
     LSI = numel(signal);
     if n < 1 || n > LSI
         throw(MException('InvalidParameter'));
