cb7a108426721f2acc8fb419830c70a8f4f00bd4
Nicolai Linhares <nicolai.linhares@gmail.com>
2013-05-09 21:49:18 -0300

    Burst detection com problemas

--- toolbox/burst_detection/bsp_burst_detect.m
+++ toolbox/burst_detection/bsp_burst_detect.m
@@ -1,23 +1,45 @@
-function [inds, rsignal] = bsp_burst_detect(signal,thres,smoothness)
-    if nargin < 2
-        thres = 0.1;
-    elseif thres < 0 || thres > 1
-        throw(MException('InvalidParameter'));
-    end
+function limites = bsp_burst_detect(signal,noiseInterval,smoothness)
     if nargin < 3;
         smoothness = 5;
     end
-    signal = signal - mean(signal);
-    env = abs(hilbert(signal));
-    env = smooth(env,'lowess',smoothness);
-    maxi = max(signal);
-    inds_noise = find(env < thres*maxi);
-    inds = find(env >= thres*maxi);
-    rsignal = env;
-    rsignal(inds_noise) = 0;
+    filt_signal = bsp_ss_filt_emd(signal,noiseInterval(1), noiseInterval(2), 1);
+    min_v = min(filt_signal);
+    max_v = max(filt_signal);
+    neg = find(signal < 0);
+    filt_signal(neg) = filt_signal(neg)*(-1);
+    env = filt_signal;
+    env = smooth(env,'lowess');
+    limiar1 = max(env)*0.05;
+    limiar2 = max(env)*0.12;
+    g = 1;
+    fechou = true;
+    burst = false;
+    for i = 1:length(env)
+        if(fechou && env(i) > limiar1)
+          temp = i;
+          fechou = false;
+        elseif(~burst && ~fechou && env(i) > limiar2)
+          limites(g) = temp;
+          g = g + 1;
+          burst = true;
+        elseif(~fechou && env(i) < limiar1)
+          if(burst)
+            limites(g) = i;
+            g = g + 1;
+          end
+          burst = false;
+          fechou = true;
+        end
+    end
+    
     if nargout == 0
-        plot(signal);
+        plot(env);
         hold on;
-        plot(env,'g');
-        plot(rsignal,'r');
+        plot(linspace(limiar1,limiar1,length(env)),'g');
+        plot(linspace(limiar2,limiar2,length(env)),'r');
+        for i = 1:2:length(limites)
+            line([limites(i);limites(i)],[min_v; max_v],'Color','magenta');
+            line([limites(i+1);limites(i+1)],[min_v; max_v],'Color','green');
+        end
+    end
     end
